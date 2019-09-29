4c5b022c6a90886d485961fa3fd630a238d92efa
Dmitry Mikhailovich <dmitry.mikhailovich@gmail.com>
2019-09-29 22:05:27 +0100

    update build to make project be compiled from terminal

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,31 @@
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.3.50"))
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.3.50"
 }
 
 repositories {
-    mavenCentral()
+    jcenter()
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.javalin:javalin:3.5.0")
-    implementation("org.slf4j:slf4j-simple:1.7.26")
-    implementation("com.github.pengrad:java-telegram-bot-api:4.4.0")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.10")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.9.10")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.10")
+    implementation("com.github.pengrad:java-telegram-bot-api:4.4.0")
+    implementation("io.javalin:javalin:3.5.0")
+    implementation("org.slf4j:slf4j-simple:1.7.26")
 
     testImplementation(kotlin("test-junit"))
 }
@@ -22,4 +34,6 @@ val jar by tasks.getting(Jar::class) {
     manifest {
         attributes["Main-Class"] = "com.dmitryvim.accounter.MainKt"
     }
+
+    from(Callable { configurations["runtimeClasspath"].map { if (it.isDirectory) it else zipTree(it) } })
 }
